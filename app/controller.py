f8d7cd9851cc816f260d877c90f26709c0d3ec8e
Barita Davitya <bdavitya@gmail.com>
2024-12-03 11:36:27 +0700

    merge controller.py

--- app/controller.py
+++ app/controller.py
@@ -9,44 +9,6 @@ def home():
 @app.route('/proses', methods=['GET', 'POST'])
 def input():
     if request.method == 'POST':
-<<<<<<< Updated upstream
-        try:
-            harga = int(request.form['harga'])  # Convert to integer
-        except ValueError:
-            harga = 0  # Handle invalid input (e.g., non-integer input)
-
-        # Ensure harga is within the valid range
-        if harga < 0:
-            harga = 0
-        elif harga > 20000000:
-            harga = 20000000
-            
-        try:
-            jarak = int(request.form['jarak'])  # Convert to integer
-        except ValueError:
-            jarak = 0  # Handle invalid input
-
-        # Ensure jarak is within the valid range
-        if jarak < 0:
-            jarak = 0
-        elif jarak > 60:
-            jarak = 60
-        
-        try:
-            waktu = float(request.form['waktu'])  # Convert to float (if it's a decimal)
-        except ValueError:
-            waktu = 0  # Handle invalid input
-
-        # Ensure waktu is within the valid range
-        if waktu < 0:
-            waktu = 0
-        elif waktu > 3:
-            waktu = 3
-            
-        # Call the inferensi function
-        result = inferensi(harga, jarak, waktu)
-        return render_template('hasil.html', jawaban=result)
-=======
         nama = request.form['nama']
 
         harga = request.form['harga']
@@ -73,7 +35,4 @@ def input():
         result, data= inferensi(hargaa, jarakk, waktuu)
         harga = f"{int(harga):,}".replace(",", ".") 
         return render_template('hasil.html', jawaban=result, nama=nama, harga=harga, jarak=jarak, waktu=waktu, data=data)
-    return render_template('input_data.html')
->>>>>>> Stashed changes
-
     return render_template('input_data.html')
